c30abecbc99c72fde7357771e45250cbef952f91
ahmedelmoselhy98 <ahmedelmoselhy98@gmail.com>
2023-11-12 21:57:47 +0200

    First

--- app/src/main/java/com/deshAndDez/ui/splash/SplashUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.deshAndDez.ui.splash
-
-sealed class SplashUiState {
-    object Initial : SplashUiState()
-    object AuthenticatedUserState :
-        SplashUiState()
-    object UnAuthenticatedUserState :
-        SplashUiState()
-}
